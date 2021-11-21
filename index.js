8a38e574955e9bee14cb78465c5694b2c862c3f1
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-21 02:39:58 -0500

    commit tree c7594ff75e0ba54047a0078a8cae883d24fe3c0f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+let userInfo = {
+    name: 'Steve',
+    email: "steve@steve.com"
+}
+
+function submitData (name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+    })
+    .then(r => r.json())
+    .then(users => renderId(users))
+    .catch((error) => {
+        document.body.innerHTML = error.message;
+        document.body.append(error)
+    
+    })
+    const renderId=users=>{
+        document.body.innerHTML = users.id;
+            }   
+}
+
